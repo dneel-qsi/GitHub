0e5746384570059c3068c6b465bb736dd8368e53
dneel-qsi <69592822+dneel-qsi@users.noreply.github.com>
2022-09-25 22:25:33 -0400

    Update LorR-working.java

--- LorR-working.java
+++ LorR-working.java
@@ -1,36 +1,3 @@
-
-New Message from Martinez, Isabel  Show  Ignore 
-
-Skip to content
-Using Gmail with screen readers
-1 of 102,316
-Super duper really hard problem that I can't solve that is hurting my feelings and making me sad
-Inbox
-
-Martinez, Isabel <isabel.martinez@stu.fayette.kyschools.us>
-8:56 PM (1 hour ago)
-to me
-
-
-
-
-Martinez, Isabel <isabel.martinez@stu.fayette.kyschools.us>
-9:10 PM (1 hour ago)
-to me
-
-
-From: Martinez, Isabel
-Sent: Sunday, September 25, 2022 3:56 PM
-To: derick.neel@gmail.com <derick.neel@gmail.com>
-Subject: Super duper really hard problem that I can't solve that is hurting my feelings and making me sad
- 
-
-
-
-Martinez, Isabel <isabel.martinez@stu.fayette.kyschools.us>
-9:16 PM (1 hour ago)
-to me
-
 // LeftOrRight.java - This program calculates the total number of left-handed and right-handed
 //                    students in a class.  
 // Input:  L for left-handed; R for right handed; X to quit.
@@ -74,11 +41,3 @@ System.out.println("Enter an L if you are left-handed,a R if you are right-hande
     } // End of main() method.
 
 } // End of LeftOrRight class.
-
-
-From: Martinez, Isabel <isabel.martinez@stu.fayette.kyschools.us>
-Sent: Sunday, September 25, 2022 4:10 PM
-To: derick.neel@gmail.com <derick.neel@gmail.com>
-Subject: Re: Super duper really hard problem that I can't solve that is hurting my feelings and making me sad
-
-Zoomed out of item.
